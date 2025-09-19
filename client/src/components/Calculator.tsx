7314fb6ad699f1a108347a9a96c3084a93682947
JoshSpiresSG <118717616+JoshSpiresSG@users.noreply.github.com>
2025-09-19 13:13:34 +0800

    Update Calculator.tsx

--- client/src/components/Calculator.tsx
+++ client/src/components/Calculator.tsx
@@ -145,7 +145,7 @@ export default function Calculator() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
-              <p className="text-sm text[#838182]">© 2025 Sphere Communications Pty. Ltd. • ABN: 61 003 102 663 • ReOC: 1114</p>
+              <p className="text-sm text-[#838182]">© 2025 Sphere Communications Pty. Ltd. • ABN: 61 003 102 663 • ReOC: 1114</p>
             </div>
           </div>
         </div>
